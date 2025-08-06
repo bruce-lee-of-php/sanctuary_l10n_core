361c8c26b1bd2192586a61ce61ebb42a082890bb
Jaylin <jaylin.bowers1@gmail.com>
2025-08-06 15:28:54 -0400

    exposed `PronounSet` and vocabulary data

--- lib/sanctuary_l10n_core.dart
+++ lib/sanctuary_l10n_core.dart
@@ -5,6 +5,10 @@
 /// grammatical gender engine.
 library sanctuary_l10n_core;
 
-// Export the public-facing models of the package.
+// Export the public-facing models
 export 'src/models/grammatical_gender.dart';
+export 'src/models/pronoun_set.dart'; // <-- Export for PronounSet
+
+// Export the public-facing classes
 export 'src/grammar_engine.dart';
+export 'src/vocabulary.dart'; // <-- Export for SanctuaryVocabulary
